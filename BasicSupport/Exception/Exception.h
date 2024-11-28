cda48fa703b816fa2762632124e70dfe6ea435af
Xiaoxuan4096 <167926415+Xiaoxuan4096@users.noreply.github.com>
2024-11-28 22:43:37 +0800

    迁移Exception库（版本：0.0.0.1）至本仓库。

---
+++ BasicSupport/Exception/Exception.h
@@ -0,0 +1,26 @@
+// Exception.h - Basic exception support.
+// Version: 0.0.0.1
+// Written by Xiaoxuan4096. All rights reserved.
+
+#pragma once
+
+#include <string>
+
+#include <Windows.h>
+
+namespace Xiaoxuan4096 {
+	namespace Exception {
+		class Exception {
+		private:
+			std::wstring detail = L"Warning: An error occurred.";
+			SYSTEMTIME time;
+		public:
+			Exception(std::wstring exceptionDetail); // Time data will be added automatically.
+
+			std::wstring getExceptionDetail();
+			SYSTEMTIME getExceptionTime();
+		};
+
+		std::wstring what(Exception exception);
+	}
+}
